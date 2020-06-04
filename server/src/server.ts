0494c3f0260cb6eab6982b551523be1095198094
almeidarenato <renatoam@ymail.com>
2020-06-03 22:49:33 -0300

    criado serviço em node para app e-coleta

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,6 +1,16 @@
 import express from "express";
+import routes from "./routes";
+import path from "path";
+import cors from "cors";
 const app = express();
-app.get("/", (req, res) => {
-  res.json({ message: "Bem vindo" });
-});
+
+/*app.use(cors({
+  origin:'wwww.domain.com'
+}))
+*/
+app.use(cors());
+app.use(express.json());
+app.use("/uploads", express.static(path.resolve(__dirname, "..", "uploads")));
+
+app.use(routes);
 app.listen(3333);
